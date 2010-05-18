4f89978b40a6977d0c74f881d85c8dd2365b57f0
Andrew Kelley <superjoe30@gmail.com>
2010-05-18 03:13:12 -0700

    Deploy script actually tests the migrations on real data

---
+++ resetdb.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+import settings
+
+"""
+this script is responsible for resetting the client database.
+"""
+
+def system(command):
+    "run a command on the system"
+    print(command)
+    code = os.system(command)
+    if code:
+        sys.exit(code)
+
+if settings.DATABASE_ENGINE == 'postgresql_psycopg2': 
+    cmd = 'echo "DROP DATABASE %s; CREATE DATABASE %s;" | psql template1' % (settings.DATABASE_NAME, settings.DATABASE_NAME)
+    system(cmd)
+else:
+    system("python manage.py reset --noinput %s" % apps)
+
+#system("cd %s; python manage.py syncdb --noinput" % old)
+
+# migrate to the server's state
+#system("cd %s; python manage.py migrate" % old)
+#for app, number in state.iteritems():
+#    system("cd %s; python manage.py migrate %s %s" % (app, number))
+
