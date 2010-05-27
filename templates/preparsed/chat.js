9968748ffd1af4c24d4250bae3acc49991ed0e18
Andrew Kelley <superjoe30@gmail.com>
2010-05-27 01:42:47 -0700

    don't try to sort onliners when there aren't any

--- templates/preparsed/chat.js
+++ templates/preparsed/chat.js
@@ -160,15 +160,17 @@ var Chat = function() {
                     return;
                 }
 
-                data.sort(function(a,b){
-                    if (a.username > b.username) {
-                        return 1;
-                    } else if (a.username < b.username) {
-                        return -1;
-                    } else {
-                        return 0;
-                    }
-                });
+                if (data.length > 0) {
+                    data.sort(function(a,b){
+                        if (a.username > b.username) {
+                            return 1;
+                        } else if (a.username < b.username) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                }
                 state.onliners = data;
                 
                 updateChatOnliners();
