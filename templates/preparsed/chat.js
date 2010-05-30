16fa77cc5db23ce62bcbfaa185bee8332d6adf8c
Andrew Kelley <superjoe30@gmail.com>
2010-05-30 11:06:46 -0700

    chat onliners: ignore case when sorting

--- templates/preparsed/chat.js
+++ templates/preparsed/chat.js
@@ -179,9 +179,11 @@ var Chat = function() {
                 
                 if (onlinerCount > 0) {
                     data.sort(function(a,b){
-                        if (a.username > b.username) {
+                        var alc = a.username.toLowerCase();
+                        var blc = b.username.toLowerCase();
+                        if (alc > blc) {
                             return 1;
-                        } else if (a.username < b.username) {
+                        } else if (alc < blc) {
                             return -1;
                         } else {
                             return 0;
